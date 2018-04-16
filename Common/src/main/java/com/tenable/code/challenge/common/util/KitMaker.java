416f1762bb84a28b47ea9b4ab76c1ec0301517e0
jawadkakar <jawadkakar@gmail.com>
2018-04-16 14:29:56 -0400

    refactored more code and add new code.

--- Common/src/main/java/com/tenable/code/challenge/common/util/KitMaker.java
+++ Common/src/main/java/com/tenable/code/challenge/common/util/KitMaker.java
@@ -1,6 +1,35 @@
 package com.tenable.code.challenge.common.util;
 
+import com.tenable.code.challenge.common.model.Kit;
+import com.tenable.code.challenge.common.model.Location;
+
+import java.util.Optional;
+
 public class KitMaker {
 
+    public static  Kit kitMaker(String id, String name, String basePrice, String bodyKit, String engine, String fourWheels, String color, String finish, String isDefaultParts,
+                         String latitude, String longitude){
 
+
+        String bPrice = getStringValue(Optional.ofNullable(basePrice));
+        String lat = getStringValue(Optional.ofNullable(latitude));
+        String lon = getStringValue(Optional.ofNullable(longitude));
+        Location location = new Location(Double.parseDouble(lat), Double.parseDouble(lon));
+        boolean defValue = getBooleanValue(Optional.ofNullable(isDefaultParts));
+
+
+        Kit kit =  new Kit(null, id, name, Double.parseDouble(bPrice), bodyKit, engine, fourWheels, color, finish, defValue, location);
+        return kit;
+    }
+
+    private static String getStringValue(Optional<String> value){
+        return value.filter(v -> value.isPresent() && value.get().matches("[0-9]+")).orElse("0");
+    }
+    private static boolean getBooleanValue(Optional<String> value){
+        boolean result = false;
+        if(value.isPresent() && value.get().matches("true")){
+            result = true;
+        }
+        return result;
+    }
 }
--- Rest/src/main/java/com/tenable/code/challenge/server/resource/CustomizationResource.java
+++ Rest/src/main/java/com/tenable/code/challenge/server/resource/CustomizationResource.java
@@ -2,7 +2,12 @@ package com.tenable.code.challenge.server.resource;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-
+import com.tenable.code.challenge.common.model.Kit;
+import com.tenable.code.challenge.common.util.KitMaker;
+import com.tenable.code.challenge.service.customization.CustomizationModule;
+import com.tenable.code.challenge.service.customization.ICustomization;
+import org.boon.json.JsonFactory;
+import org.boon.json.ObjectMapper;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,14 +15,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Optional;
-
-import com.tenable.code.challenge.common.model.Kit;
-import com.tenable.code.challenge.common.model.Location;
-import com.tenable.code.challenge.service.customization.CustomizationModule;
-import com.tenable.code.challenge.service.customization.ICustomization;
-import org.boon.json.JsonFactory;
-import org.boon.json.ObjectMapper;
 
 @Path("/customize")
 public class CustomizationResource {
@@ -29,54 +26,23 @@ public class CustomizationResource {
     @Path("/kitcustomizer")
     @Produces(MediaType.APPLICATION_JSON)
     public Response customize(@QueryParam("id") String id,
-                             @QueryParam("name") String name,
-                             @QueryParam("basePrice") String basePrice,
-                             @QueryParam("bodyKit") String bodyKit,
-                             @QueryParam("engine") String engine,
-                             @QueryParam("fourWheels") String fourWheels,
-                             @QueryParam("color") String color,
-                             @QueryParam("finish") String finish,
-                             @QueryParam("isDefaultParts") String isDefaultParts,
-                             @QueryParam("latitude") String latitude,
-                             @QueryParam("longitude") String longitude
-                             ) {
+                              @QueryParam("name") String name,
+                              @QueryParam("basePrice") String basePrice,
+                              @QueryParam("bodyKit") String bodyKit,
+                              @QueryParam("engine") String engine,
+                              @QueryParam("fourWheels") String fourWheels,
+                              @QueryParam("color") String color,
+                              @QueryParam("finish") String finish,
+                              @QueryParam("isDefaultParts") String isDefaultParts,
+                              @QueryParam("latitude") String latitude,
+                              @QueryParam("longitude") String longitude
+    ) {
         System.out.println("Hello");
         /** Create a Kit from Rest parameters**/
-        Kit kit = kitMaker(id, name, basePrice, bodyKit, engine, fourWheels, color, finish, isDefaultParts, latitude, longitude);
+        Kit kit = KitMaker.kitMaker(id, name, basePrice, bodyKit, engine, fourWheels, color, finish, isDefaultParts, latitude, longitude);
         Kit kitCust = customization.customize(kit);
         String sr = mapper.toJson(kitCust);
         return Response.status(200).entity(sr).build();
-
     }
 
-    private Kit kitMaker(String id, String name, String basePrice, String bodyKit, String engine, String fourWheels, String color, String finish, String isDefaultParts,
-                         String latitude, String longitude){
-
-
-        String bPrice = getStringValue(Optional.ofNullable(basePrice));
-        String lat = getStringValue(Optional.ofNullable(latitude));
-        String lon = getStringValue(Optional.ofNullable(longitude));
-        Location location = new Location(Double.parseDouble(lat), Double.parseDouble(lon));
-        boolean defValue = getBooleanValue(Optional.ofNullable(isDefaultParts));
-
-
-        Kit kit =  new Kit(null, id, name, Double.parseDouble(bPrice), bodyKit, engine, fourWheels, color, finish, defValue, location);
-        return kit;
-    }
-
-    private String getStringValue(Optional<String> value){
-        return value.filter(v -> value.isPresent() && value.get().matches("[0-9]+")).orElse("0");
-    }
-    private boolean getBooleanValue(Optional<String> value){
-        boolean result = false;
-        if(value.isPresent() && value.get().matches("true")){
-            result = true;
-        }
-        return result;
-    }
-    public static void main(String[] args) {
-        Optional<Double> basePriceOptional = Optional.ofNullable(null);
-        Double bbPrice = basePriceOptional.filter(bp -> bp.isNaN()).orElse(Double.valueOf("0"));
-        System.out.println(bbPrice);
-    }
 }
